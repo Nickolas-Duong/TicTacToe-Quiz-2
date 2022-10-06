fc03d303deafe11d6bce6e624b72a1ed9f090310
Nickolas Duong <nduong@cpp.edu>
2022-10-05 19:33:19 -0700

    Add initial blueprint for Tic Tac Toe Game as comments

---
+++ Function.c
@@ -0,0 +1,5 @@
+//Add header file to Function.c
+
+//function for player choosing location
+
+//function for random choice by AI
---
+++ tictactoe.c
@@ -0,0 +1,44 @@
+//Create a Tic Tac Toe Game where the user can play alone or with another player.
+//Game will proceed to play until cat's scratch, win, or lose
+
+//include system, string, time function, header with functions
+//enum win condition?
+
+//create main
+
+//variables
+//int p1Choice
+//int p2Choice
+//char col[]
+//char row[]
+//bool complete
+
+//while loop to get chosen user choice
+//Print out user menu
+
+//Ask for user input
+
+//determine game choice based on user choice (prevent any choice not allowed)
+
+//switch based on choice
+//end while loop
+
+//case 1: Player v AI
+//while loop where player plays the game against an AI
+//condition = Enum status
+
+//player inputs location via numbers for column | rows (prevent choosing a location already taken)
+
+//AI then randomly chooses an option, re rolling when spot is occupied
+
+//continue play until completion
+
+//if else for enum
+//change enum to game result
+//print result of game
+//break from loop
+
+//case 2: player v player
+//Repeat case 1, except allow player 2 to input choices instead of AI
+
+//end program
